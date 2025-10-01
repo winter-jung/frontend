63b87660f1a842e09f35bac3257837988a7ba5a6
tpwjd123 <tpwjdskql@gmail.com>
2025-10-01 16:46:13 +0900

    header수정-김세정

--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -3,13 +3,30 @@ import { Link, useNavigate } from "react-router";
 function Navi() {
     const back = useNavigate(-1);
     return (
-        <div className="flex gap-8 bg-pink-100 p-4 text-[20px]">
-            <Link to="/">
-                <h1 className='w-[270px]'><img src="./morebomb_logo.svg" alt="logo" /></h1>
-            </Link>
-            <Link to={`/genres/18`}>드라마</Link>
-            <Link to={`/genres/10759`}>액션 & 어드벤처</Link>
-            <Link to={`/genres/16`}>애니메이션</Link>
+        <div className="fixed top-0 left-0 w-full py-8 bg-black/90 z-50 backdrop-blur-lg" 
+        style={{background: 'linear-gradient(to bottom, #0d0727 0%, rgba(13,7,39,0.6) 87.5%, rgba(13,7,39,0.4) 100%)'}}>
+            <div className="container mx-auto flex whitespace-nowrap justify-between items-center">
+                <div className="flex items-center gap-20">
+                    <Link to="/">
+                        <h1 className='w-[270px]'><img src="./morebomb_logo.svg" alt="logo" /></h1>
+                    </Link>
+                    <div className="flex gap-8 p-4 text-white text-[20px] justify-center ">
+                        <Link to={`/`}>홈</Link>
+                        <Link to={`/genres/18`}>드라마</Link>
+                        <Link to={`/genres/16`}>영화</Link>
+                        <Link to={`/genres/10759`}>애니</Link>
+                        <Link to={`/genres/18`}>예능</Link>
+                        <Link to={`/genres/10759`}>몰밤각</Link>
+                        <Link to={`/genres/16`}>요즘 대세 콘텐츠</Link>
+                    </div>
+                </div>
+                <div className='flex gap-3'>
+                    <Link><img src="./search.svg" alt="search" /></Link>
+                    <Link><img src="./noti.svg" alt="noti" /></Link>
+                    <Link><img src="./people.svg" alt="people" /></Link>
+                </div>
+            </div>
+
         </div>
     )
 }
