992b97e1aa958e1f01dbd5b9695914b2b125f4f7
tpwjd123 <tpwjdskql@gmail.com>
2025-10-02 18:34:20 +0900

    비디오경로수정

--- src/App.jsx
+++ src/App.jsx
@@ -126,7 +126,7 @@ function VideoHero() {
   return (
     <section className="relative h-screen overflow-hidden">
       <video autoPlay  muted={muted} loop playsInline className='absolute top-0 left-0 w-full h-full object-cover'>
-        <source src='/public/video.mp4' />
+        <source src='video.mp4' />
       </video>
       <div className="relative z-10 flex-col flex items-center justify-center h-full mt-65">
         <button
