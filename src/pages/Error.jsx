c156570043d2f05b30136c201e12ec7774fb826f
winter-jung <wkdpffl@naver.com>
2025-10-10 14:23:04 +0900

    [winter]-error페이지 꾸미기 및 수정

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,16 @@
 import Navi from "./Navi";
+import StarBackground from '../components/StarBackground';
+
 const Error = function () {
     return (
-        <div>
-            <Navi />
-            <div className="py-10 w-screen h-screen text-[5vw] text-white bg-black">
-                <h1>404 Error</h1>
+        <div className="relative min-h-screen">
+            <StarBackground className="absolute inset-0" />
+            <div className="fixed top-0 left-0 right-0 z-50">
+                <Navi />
+            </div>
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+                <h1 className="text-[5vw] font-bold z-10">404 Error</h1>
+                <p className="text-[2vw] mt-4 z-10">🌠페이지를 찾을 수 없습니다.🌠</p>
             </div>
         </div>
     )
